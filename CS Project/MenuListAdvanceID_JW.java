fd67a4e94aaa8c64993601cb21975719c40657ae
James Wu <Jwu96@hawk.iit.edu>
2018-04-13 14:32:51 -0500

    Add files via upload

---
+++ CS Project/MenuListAdvanceID_JW.java
@@ -0,0 +1,18 @@
+public class MenuListAdvance_IDJW extends MenuList_IDJW{
+  public MenuListAdvance_IDJW(){
+    super();
+  }
+  public MenuListAdvance_IDJW(int num){
+    super(num);
+  }
+  public MenuListAdvance_IDJW(Restaurant_IDJW[] arr4, int indexNext){
+    super(arr4, indexNext);
+  }
+  public void sort(){
+
+  }
+  @Override
+  public void isThere(){
+    //Use Binary Search
+  }
+}
